07c4996eb434050916e1df94ad6d799517528c0a
Antoine Rousselot <antoine.rousselotvigier@centrale-marseille.fr>
2018-10-12 10:23:50 +0200

    MVC5 - getPages()

--- project/src/test/java/fr/cmm/helper/PaginationTest.java
+++ project/src/test/java/fr/cmm/helper/PaginationTest.java
@@ -3,7 +3,11 @@ package fr.cmm.helper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
+
 
 public class PaginationTest {
 
@@ -17,6 +21,14 @@ public class PaginationTest {
         Assert.assertEquals(3, pagination.getPageCount());
         pagination.setCount(0);
         Assert.assertEquals(1, pagination.getPageCount());
+    }
 
+    @Test
+    public void getPages(){
+        Pagination pagination=new Pagination();
+        pagination.setCount(250);
+        pagination.setPageSize(50);
+        List<Integer> myTestList= Arrays.asList(1,2,3,4,5);
+        Assert.assertEquals(myTestList,pagination.getPages());
     }
 }
